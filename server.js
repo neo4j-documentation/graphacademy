f1f34e7d0f5eaa3b69239c872422563381edb849
Adam Cowley <github@adamcowley.co.uk>
2020-08-28 18:25:04 +0100

    Initial content and basic user guide

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const serveIndex = require('serve-index')
 
 const app = express()
 app.use(express.static('./build/site'))
 
 app.use('/static/assets', express.static('./build/site/_'))
-
+app.use('/', serveIndex('./build/site'))
 
 app.listen(8000, () => console.log('📘 http://localhost:8000'))
